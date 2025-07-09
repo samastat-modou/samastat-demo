0cc03e772df5920c523ea6bb8e82b1bfc462b72e
samastat-modou-sene <semodou55@gmail.com>
2025-07-09 12:37:50 +0000

    Update samastat_app_stream.py

--- samastat_app_stream.py
+++ samastat_app_stream.py
@@ -1,5 +1,10 @@
 
 import streamlit as st
+from PIL import Image
+# Charger et afficher le logo
+logo = Image.open("assets/logo_samastat.png")  # place ton logo dans un dossier assets/
+st.image(logo, width=120)
+
 import pandas as pd
 import numpy as np
 import plotly.express as px
